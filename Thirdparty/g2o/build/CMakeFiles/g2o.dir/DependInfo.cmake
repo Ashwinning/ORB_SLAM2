82b3a4a263cf46f775cca54d7fc5bf53970d4954
Ashwin Sinha <ash@ds.gg>
2016-03-31 16:56:21 -0700

    Removed ORBvoc.txt
    Original Commit Message Follows :
    
    DBoW2 Compiled!
    
    - Changed an include in fobb.cpp to import ints and make it MSVC
    compatible

---
+++ Thirdparty/g2o/build/CMakeFiles/g2o.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/stuff/os_specific.c" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/os_specific.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../core"
+  "../types"
+  "../stuff"
+  "C:/Program Files/PCL 1.7.2/3rdParty/Eigen/eigen3"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/batch_stats.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/batch_stats.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/cache.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/cache.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/estimate_propagator.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/estimate_propagator.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/factory.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/factory.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/hyper_dijkstra.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_dijkstra.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/hyper_graph.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/hyper_graph_action.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph_action.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/jacobian_workspace.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/jacobian_workspace.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/marginal_covariance_cholesky.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/marginal_covariance_cholesky.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/matrix_structure.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/matrix_structure.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/optimizable_graph.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimizable_graph.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/optimization_algorithm.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/optimization_algorithm_factory.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_factory.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/optimization_algorithm_levenberg.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_levenberg.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/optimization_algorithm_with_hessian.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_with_hessian.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/parameter.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/parameter_container.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter_container.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/robust_kernel.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/robust_kernel_factory.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_factory.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/robust_kernel_impl.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_impl.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/solver.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/solver.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/core/sparse_optimizer.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/sparse_optimizer.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/stuff/property.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/property.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/stuff/string_tools.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/string_tools.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/stuff/timeutil.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/timeutil.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/types/types_sba.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_sba.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/types/types_seven_dof_expmap.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_seven_dof_expmap.cpp.obj"
+  "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/g2o/types/types_six_dof_expmap.cpp" "C:/Users/Ashwin/Documents/GitHub/ORB_SLAM2/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_six_dof_expmap.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../core"
+  "../types"
+  "../stuff"
+  "C:/Program Files/PCL 1.7.2/3rdParty/Eigen/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
